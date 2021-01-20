a9f7d0aac8f0750baab4e01a150e5df86369f103
Iman Sobhaniyan <sobhaniyan.iman@ymail.com>
2021-01-20 15:00:33 +0330

    init dbContext

---
+++ Ighan.Analitics.DataAccessLayer/AnaliticsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ighan.Analitics.StorageModels;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ighan.Analitics.DataAccessLayer
+{
+    public class AnaliticsDbContext : DbContext
+    {
+        public AnaliticsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Device> Devices { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
